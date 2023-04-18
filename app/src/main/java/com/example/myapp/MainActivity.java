696c9dba5adc0d994fb22005464af30096ee1074
User <ninth009@mail.ru>
2023-04-18 18:49:22 +0700

    Application

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webview=(WebView) findViewById(R.id.wv_table);
-        webview.setWebViewClient(new WebViewClient());
-        webview.loadUrl("https://timetable.tusur.ru/faculties/fvs/groups/590-1");
+        RecyclerView recyclerView=(RecyclerView) findViewById(R.id.wv_table);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        TableAdapt tableAdapt=new TableAdapt(this,createList());
+        recyclerView.setAdapter(tableAdapt);
+    }
+    private List<ListItem> createList()
+    {
+        List<ListItem> result=new ArrayList<>();
+        result.add(new ListItem("ММИС","8:50\n10:25","пн, 17 апр."));
+        result.add(new ListItem("ММИС","10:40\n12:15","вт"));
+        return result;
     }
 }
--- app/src/main/java/com/example/myapp/TableAdapt.java
+++ app/src/main/java/com/example/myapp/TableAdapt.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class TableAdapt extends RecyclerView.Adapter<TableAdapt.TableAdaptViewHolder>
 {
     private final LayoutInflater layoutInflater;
@@ -25,21 +27,36 @@ public class TableAdapt extends RecyclerView.Adapter<TableAdapt.TableAdaptViewHo
     public TableAdaptViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
         View view=layoutInflater.inflate(R.layout.table_i,parent,false);
-        return null;
+        return new TableAdaptViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TableAdaptViewHolder holder, int position)
     {
         ListItem listItem=listItemList.get(position);
-        holder.dayOfWeek.setText(listItem.get_day());
-        holder.lesson.setText(listItem.get_day());
-        holder.timeOfLessons.setText(listItem.get_day());
+
+        holder.dayOfWeek.setText(listItem.getDayOfWeek());
+
+        holder.lesson.setText(listItem.getFirstLesson());
+        holder.lesson.setText(listItem.getSecondLesson());
+        holder.lesson.setText(listItem.getThirdLesson());
+        holder.lesson.setText(listItem.getFourthLesson());
+        holder.lesson.setText(listItem.getFifthLesson());
+        holder.lesson.setText(listItem.getSixthLesson());
+        holder.lesson.setText(listItem.getSeventhLesson());
+
+        holder.timeOfLessons.setText(listItem.getTimeFirstLesson());
+        holder.timeOfLessons.setText(listItem.getTimeSecondLesson());
+        holder.timeOfLessons.setText(listItem.getTimeThirdLesson());
+        holder.timeOfLessons.setText(listItem.getTimeFourthLesson());
+        holder.timeOfLessons.setText(listItem.getTimeFifthLesson());
+        holder.timeOfLessons.setText(listItem.getTimeSixthLesson());
+        holder.timeOfLessons.setText(listItem.getTimeSeventhLesson());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listItemList.size();
     }
 
     public class TableAdaptViewHolder extends RecyclerView.ViewHolder
@@ -51,8 +68,10 @@ public class TableAdapt extends RecyclerView.Adapter<TableAdapt.TableAdaptViewHo
         {
             super(view);
             dayOfWeek=view.findViewById(R.id.tv_day);
+
             timeOfLessons=view.findViewById(R.id.tv_time_first_lesson);
-            lesson=view.findViewById(R.id.tv_lesson);
+
+            lesson=view.findViewById(R.id.tv_lesson_one);
         }
     }
 }
